17595335dcae805f3264f483bb68197577065a14
wuriyanto <wuriyanto48@yahoo.co.id>
2017-12-10 02:49:46 +0700

    add is valid password func

--- modules/auth/model/identity.go
+++ modules/auth/model/identity.go
@@ -1,8 +1,17 @@
 package model
 
+import "errors"
+
 type Identity struct {
-	ID           string
-	Email        string
-	Password     string
-	PasswordSalt string
+	ID           string `json:"id"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	PasswordSalt string `json:"passwordSalt"`
+}
+
+func (i *Identity) IsValidPassword(password string) error {
+	if i.Password != password {
+		return errors.New("Password is not valid")
+	}
+	return nil
 }
