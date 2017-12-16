29065a8ebd8701bf35c7aa535dfb866555890d01
wuriyanto <wuriyanto48@yahoo.co.id>
2017-12-17 00:31:58 +0700

    add http request async

--- utils/http_request.go
+++ utils/http_request.go
@@ -35,8 +35,13 @@ func (c *httpRequest) newReq(method string, fullPath string, body io.Reader, hea
 	return req, nil
 }
 
-// exec function, for execute http request
-func (c *httpRequest) exec(req *http.Request, v interface{}) error {
+// Req public function for call http request
+func (c *httpRequest) Req(method, path string, body io.Reader, v interface{}, headers map[string]string) error {
+	req, err := c.newReq(method, path, body, headers)
+
+	if err != nil {
+		return err
+	}
 
 	res, err := c.httpClient.Do(req)
 	defer res.Body.Close()
@@ -52,17 +57,30 @@ func (c *httpRequest) exec(req *http.Request, v interface{}) error {
 	return nil
 }
 
-// Req public function for call http request
-func (c *httpRequest) Req(method, path string, body io.Reader, v interface{}, headers map[string]string) error {
-	req, err := c.newReq(method, path, body, headers)
+// ReqAsync public function for call http request with async
+func (c *httpRequest) ReqAsync(method, path string, body io.Reader, v interface{}, headers map[string]string) <-chan error {
+	output := make(chan error, 1)
+	go func() {
+		req, err := c.newReq(method, path, body, headers)
 
-	if err != nil {
-		return err
-	}
+		if err != nil {
+			output <- err
+			return
+		}
 
-	if err := c.exec(req, v); err != nil {
-		return err
-	}
+		res, err := c.httpClient.Do(req)
+		defer res.Body.Close()
 
-	return nil
+		if err != nil {
+			output <- err
+			return
+		}
+
+		if v != nil {
+			output <- json.NewDecoder(res.Body).Decode(v)
+			return
+		}
+
+	}()
+	return output
 }
