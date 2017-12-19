29a1b8f6f66594a08caafd78f3299339a7e27749
wuriyanto <wuriyanto48@yahoo.co.id>
2017-12-20 00:49:55 +0700

    add bearer test case

--- middleware/bearer_test.go
+++ middleware/bearer_test.go
@@ -18,6 +18,7 @@ func TestBearer(t *testing.T) {
 	validAccessToken := `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJQV1QiLCJpYXQiOjE1MTIzMTg0MjMsImlzcyI6InB1cndva2VydG9kZXYuZ2l0aHViLmlvIiwic3ViIjoiMDAxIn0.GeEDHv82F8xp_98QQLiWxZ5aVBzZej0e-Ios8M9l0tYdrrTbdP3zxutSi5H7rxrd43PmlFi0pMMGtbVw64kPkBspCE3Kebbeersa8isn1zBejZO62mIgpRIGRhAJ_rphsxXYqOKKQlgj2ecI39dRR7IRJZdNYoTXXeBktUeUcDU`
 	expiredToken := `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJQV1QiLCJleHAiOjE1MTIzMjA1NzcsImlhdCI6MTUxMjMyMDU2NywiaXNzIjoicHVyd29rZXJ0b2Rldi5naXRodWIuaW8iLCJzdWIiOiIwMDEifQ.WLrCMOaQu0pb2XKdZOpZp81_9wJ7vAm3GKhA0h8lbioF5kqHVzmlNjnkJ1ayjYQMTNfYhhONX8IHOCCoMGsG5xibKG88_ICHQHbOtFOINaTIxpewmRbDlbrg8BA8aihXQD-L8a_Nm5ZYgpHAcCZ02m0MNSwHYwHBYYbHpnvqRIo`
 	noHeaderToken := `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJQV1QiLCJleHAiOjE1MTIzMjA1NzcsImlhdCI6MTUxMjMyMDU2NywiaXNzIjoicHVyd29rZXJ0b2Rldi5naXRodWIuaW8iLCJzdWIiOiIwMDEifQ.WLrCMOaQu0pb2XKdZOpZp81_9wJ7vAm3GKhA0h8lbioF5kqHVzmlNjnkJ1ayjYQMTNfYhhONX8IHOCCoMGsG5xibKG88_ICHQHbOtFOINaTIxpewmRbDlbrg8BA8aihXQD-L8a_Nm5ZYgpHAcCZ02m0MNSwHYwHBYYbHpnvqRIo`
+	invalidHeaderToken := `Basic eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJQV1QiLCJleHAiOjE1MTIzMjA1NzcsImlhdCI6MTUxMjMyMDU2NywiaXNzIjoicHVyd29rZXJ0b2Rldi5naXRodWIuaW8iLCJzdWIiOiIwMDEifQ.WLrCMOaQu0pb2XKdZOpZp81_9wJ7vAm3GKhA0h8lbioF5kqHVzmlNjnkJ1ayjYQMTNfYhhONX8IHOCCoMGsG5xibKG88_ICHQHbOtFOINaTIxpewmRbDlbrg8BA8aihXQD-L8a_Nm5ZYgpHAcCZ02m0MNSwHYwHBYYbHpnvqRIo`
 	invalidToken := `abcd3`
 	t.Run("Test Valid Access Token", func(t *testing.T) {
 
@@ -71,6 +72,29 @@ func TestBearer(t *testing.T) {
 		req := httptest.NewRequest("GET", "/", nil)
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Authorization", noHeaderToken)
+		rec := httptest.NewRecorder()
+
+		verifyKey, _ := getPublicKey(PublicKey)
+
+		testHandler := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+
+			res.WriteHeader(http.StatusOK)
+			res.Header().Set("Content-Type", "application/json")
+
+			io.WriteString(res, `{"alive": true}`)
+		})
+
+		handler := Bearer(verifyKey, testHandler)
+		handler.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusUnauthorized, rec.Code)
+	})
+
+	t.Run("Test Access Token with Invalid Header", func(t *testing.T) {
+
+		req := httptest.NewRequest("GET", "/", nil)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", invalidHeaderToken)
 		rec := httptest.NewRecorder()
 
 		verifyKey, _ := getPublicKey(PublicKey)
